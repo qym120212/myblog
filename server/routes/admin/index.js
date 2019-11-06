b0247d58f608c5df4d498b22cefd8886267d256c
qym120212 <qym3009@foxmail.com>
2019-11-06 20:18:23 +0800

    upload

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -131,7 +131,7 @@ module.exports = app =>{
     const upload = multer({dest:__dirname + '/../../uploads'})
     app.post('/admin/api/upload',upload.single('file'),auth,async(req,res)=>{
         const file = req.file
-        file.url = `http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://quyuanming/uploads/${file.filename}`
         res.send(file)
     })
 
