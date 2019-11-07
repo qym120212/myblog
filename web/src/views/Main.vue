096d103dbbcf98af7e9c1b5706aa04d11348a642
qym120212 <qym3009@foxmail.com>
2019-11-07 21:37:43 +0800

    手机端适配

--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -18,9 +18,9 @@
     </div>
     <div class="footer">
       <div class="webdetail">
-        <span>ICP备案：苏ICP备19034670号-1</span>
-      <span><img src="../assets/beian.png" alt="">
-        苏公网安备 32058102001549号</span><a href=""></a>
+        <div>ICP备案：苏ICP备19034670号-1</div>
+      <div><img src="../assets/beian.png" alt="">
+        苏公网安备 32058102001549号</div><a href=""></a>
       </div>
     </div>
   </div>
@@ -30,27 +30,12 @@ import "../css/Animate.css";
 export default {};
 </script>
 <style>
-@media screen and (max-width: 500px){
-  .header{
-    height: 60px !important;
-    margin: 0 auto 15px !important
-  }
-  .header .logo img{
-    margin-top: 10px;
-    height: 40px !important;
-  }
-  .link{
-    width: 70% !important;
-    font-size: 14px;
-    padding: 10px;
-   }
-}
 .header {
-  height: 90px;
+  height: 5.625rem;
   width: 90%;
   margin: auto;
-  border-bottom: .5px solid #e4e4e4;
-  font-size: 18px
+  border-bottom: 0.0313rem solid #e4e4e4;
+  font-size: 1.125rem
 }
 .header .router-link-active{
   color: #000 !important;
@@ -62,7 +47,7 @@ export default {};
   width: 40%;
   display: flex;
   justify-content: space-around;
-  padding-top: 20px;
+  padding-top: 1.25rem;
 }
 a {
   color: #999595;
@@ -72,12 +57,12 @@ a:hover {
   color: #f54d4d;
 }
 .logo{
-  margin-top: 20px;
-  margin-left: 30px;
+  margin-top: 1.25rem;
+  margin-left: 1.875rem;
 }
 .logo img {
   position: absolute;
-  height: 60px;
+  height: 3.75rem;
   border-radius: 50%;
 }
 .display {
@@ -85,32 +70,61 @@ a:hover {
   font-weight: bold;
 }
 .otherlink img {
-  height: 25px;
+  height: 1.5625rem;
   border-radius: 50%;
 }
 .router-link-active:nth-of-type(1) {
-  border-bottom: solid rgb(255, 184, 126) 3px;
+  border-bottom: solid rgb(255, 184, 126) 0.1875rem;
 }
 .router-link-active:nth-of-type(2) {
-  border-bottom: solid rgb(17, 118, 235) 3px;
+  border-bottom: solid rgb(17, 118, 235) 0.1875rem;
 }
 .router-link-active:nth-of-type(3) {
-  border-bottom: solid rgb(255, 126, 159) 3px;
+  border-bottom: solid rgb(255, 126, 159) 0.1875rem;
 }
 .router-link-active:nth-of-type(4) {
-  border-bottom: solid rgb(184, 93, 212) 3px;
+  border-bottom: solid rgb(184, 93, 212) 0.1875rem;
 }
 .container{
-  height: 1500px;
+  height: 93.75rem;
 }
 .footer{
-  height: 70px;
+  height: 4.375rem;
   background-color: #ebebeb;
 }
 .webdetail{
-  padding: 20px 350px;
-    display: flex;
+  padding: 1.25rem 21.875rem;
+  display: flex;
   justify-content: space-around;
   align-items: center;
+}
+@media screen and (max-width: 500px){
+  .header{
+    height: 3.125rem;
+    margin: 0 auto 0.9375rem;
+  }
+  .header .logo img{
+    height: 2.5rem;
+  }
+  .link{
+    width: 60% ;
+    font-size: 0.875rem;
+    padding: 0.625rem;
+   }
+   a{
+     font-size: 1.125rem;
+   }
+   .footer{
+     height: 2.5rem;
+   }
+   .webdetail{
+     padding: 0;
+     display: block;
+   }
+   .webdetail div{
+     display: flex;
+     justify-content: center;
+   }
+
 }
 </style>
